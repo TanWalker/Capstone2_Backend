648d9ed676d4f39107347dba4c3dc0b25c09766e
Walker <walker@enclave.vn>
2019-02-25 10:24:17 +0700

    merge Walker to dev

--- backend/middleware/AuthGuard.js
+++ backend/middleware/AuthGuard.js
@@ -1,34 +1,30 @@
-const jwt = require("jsonwebtoken"),
+const jwt = require('jsonwebtoken');
 const Constants = require('../libs/Constants');
 const config = require('config');
 
 module.exports = (req, res, next) => {
-    var token = req.headers['authorization'];
-    if (!token) 
-      return res.status(401).send({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token,config.get('token_key'), (err, decoded) => {
+  var token = req.headers['authorization'];
+  if (!token)
+    return res.status(401).send({ auth: false, message: 'No token provided.' });
 
-      // check for error
-      if (!err) {
+  jwt.verify(token, config.get('token_key'), (err, decoded) => {
+    // check for error
+    if (!err) {
+      // get expired date
+      var dateNow = parseInt(new Date().getTime() / 1000);
+      var expiryDate = +decoded.iat + Constants.EXPIRES * 60 * 60;
 
-        // get expired date
-        var dateNow = parseInt(new Date().getTime() / 1000);
-        var expiryDate = +decoded.iat + (Constants.EXPIRES * 60 * 60);
-
-        // check expiration
-        if (dateNow < expiryDate) {
-
-          // set user
-          req.user = decoded;
-
-        }
-      } else {
-        console.log("Error in auth guard: ");
-        console.log(err);
+      // check expiration
+      if (dateNow < expiryDate) {
+        // set user
+        req.user = decoded;
       }
+    } else {
+      console.log('Error in auth guard: ');
+      console.log(err);
+    }
 
-      // keep moving
-      next();
-    });
-}
+    // keep moving
+    next();
+  });
+};
