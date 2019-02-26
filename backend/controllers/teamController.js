8a99faa77bf8f6a5ff62961aa1f999eb3e5bd1c7
Triton <triton@enclave.vn>
2019-02-26 17:02:10 +0700

    Update team create

--- backend/controllers/teamController.js
+++ backend/controllers/teamController.js
@@ -5,48 +5,38 @@ const ReturnResult = require('../libs/ReturnResult');
 const config = require('config');
 
 exports.Register = (req, res, next) => {
-    var params = req.body;
-    var data = user_md.findOne({ where: { name: params.name } });
-    //check whether existing user
-    data.then(function(data) {
-      if (data) {
-        return res.json(
-          new ReturnResult('Error', null, null, Constants.messages.EXISTING_USER)
-        );
-      } else {
-        //hash password
-        bcrypt.hash(params.password, 10).then(function(password) {
-          //Insert user to database
-          var result = user_md.create({
-            username: params.username,
-            email: params.email,
-            password: password,
-            first_name: params.firstname,
-            last_name: params.lastname,
-            dob: params.dateofbirth,
-            phone_num: params.phone_num,
-            role_id: params.role_id
-          });
-          result
-            .then(function(user) {
-              var result = {
-                user: user
-              };
-              res
-                .status(200)
-                .json(new ReturnResult(null, result, 'User Created', null));
-            })
-            .catch(function(err) {
-              res.json(
-                new ReturnResult(
-                  'Error',
-                  null,
-                  null,
-                  Constants.messages.INVALID_INFORMATION
-                )
-              );
-            });
+  var params = req.body;
+  var data = user_md.findOne({ where: { name: params.name } });
+  //check whether existing user
+  data.then(function(data) {
+    if (data) {
+      return res.json(
+        new ReturnResult('Error', null, null, Constants.messages.EXISTING_TEAM)
+      );
+    } else {
+      //Insert team to database
+      var result = user_md.create({
+          
+      });
+      result
+        .then(function(user) {
+          var result = {
+            user: user
+          };
+          res
+            .status(200)
+            .json(new ReturnResult(null, result, 'User Created', null));
+        })
+        .catch(function(err) {
+          res.json(
+            new ReturnResult(
+              'Error',
+              null,
+              null,
+              Constants.messages.INVALID_INFORMATION
+            )
+          );
         });
-      }
-    });
-  };
+    }
+  });
+};
