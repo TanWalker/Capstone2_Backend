934962b60737e872a549b453bce885b9c17cb846
Kaiser <phamanhkhoa0@gmail.com>
2019-03-04 15:35:31 +0700

    update exercise

--- backend/routes/swimRoutes.js
+++
@@ -1,9 +0,0 @@
-const swimController = require('../controllers/swimController');
-const express = require('express');
-const router = express.Router();
-const authCheck = require('../middleware/AuthGuard');
-
-router.get('/api/public/getSwimStyle', swimController.getSwimStyle);
-
-
-module.exports = router;
