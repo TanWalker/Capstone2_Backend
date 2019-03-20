abf176c74e9a13d6c68f56ef59eb563d136ab4a8
kaiser2552 <47590931+kaiser2552@users.noreply.github.com>
2019-03-20 14:11:37 +0700

    Update AuthGuard.js

--- backend/middleware/AuthGuard.js
+++ backend/middleware/AuthGuard.js
@@ -5,8 +5,8 @@ const ReturnResult = require('../libs/ReturnResult');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    // var token = req.headers['authorization'];
+    //const token = req.headers.authorization.split(' ')[1];
+    var token = req.headers['authorization'];
     const decodedToken = jwt.verify(token, config.get('token_key'));
     req.userData = {
       id: decodedToken.id,
